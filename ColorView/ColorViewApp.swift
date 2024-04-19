f6c727da6918a2be98bbb2451d0d8929860e39dd
Aleksey Konchyts <konchits.aleksey@gmail.com>
2024-04-19 12:33:31 +0300

    Initial Commit

---
+++ ColorView/ColorViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewApp.swift
+//  ColorView
+//
+//  Created by Aleksey Konchyts on 19.04.24.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
